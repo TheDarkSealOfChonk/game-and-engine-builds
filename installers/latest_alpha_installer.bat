d29edee0aa23a8d30a4ce8ac895685f89b1805c6
Your Name <you@example.com>
2024-04-29 00:50:06 -0400

    eeee

---
+++ installers/latest_alpha_installer.bat
@@ -0,0 +1,6 @@
+curl "https://raw.githubusercontent.com/TheDarkSealOfChonk/game-and-engine-builds/main/game-builds/latest_alpha.zip" -OutFile "latest_alpha.zip"
+powershell.exe -Command "Expand-Archive -Path 'latest_alpha.zip' -DestinationPath 'latest_alpha'"
+del "latest_alpha.zip"
+cd latest_alpha
+cd latest_alpha
+.\"Egon Rise Of The Angels.exe"
