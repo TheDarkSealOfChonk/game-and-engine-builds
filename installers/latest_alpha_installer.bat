0809e9450f156003e1fa2b2011f6e1d198681fe1
Your Name <you@example.com>
2024-04-29 01:37:42 -0400

    Update latest_alpha_installer.bat

--- installers/latest_alpha_installer.bat
+++ installers/latest_alpha_installer.bat
@@ -1,6 +1,5 @@
-del "latest_alpha"
 curl "https://raw.githubusercontent.com/TheDarkSealOfChonk/game-and-engine-builds/main/game-builds/latest_alpha.zip" -OutFile "latest_alpha.zip"
-powershell.exe -Command "Expand-Archive -Path 'latest_alpha.zip' -DestinationPath 'latest_alpha'"
+powershell.exe -Command "Expand-Archive -Force -Path 'latest_alpha.zip' -DestinationPath 'latest_alpha'"
 del "latest_alpha.zip"
 move latest_alpha\run.bat run.bat
 .\run
